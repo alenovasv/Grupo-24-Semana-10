73d920cab6c91dacc41ca3c76ee5a2e7b6f2b199
alenovasv <116722421+alenovasv@users.noreply.github.com>
2025-09-24 22:19:48 -0600

    Add files via upload

---
+++ Script de UPDATE tabla Cliente.sql
@@ -0,0 +1,174 @@
+
+UPDATE CLIENTE 
+
+SET nombre = "Marianela" , apellido = "Villeda ", correo = "maria125ville@gmail.com", telefono = "7025-2514" 
+
+Where id_cliente = 1; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Pepe" , apellido = "Aguilar ", correo = "pepeaguilar@outlook.es", telefono = "7476-5681" 
+
+Where id_cliente = 3; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Oscar" , apellido = "Guzman ", correo = "Edificacionesguzman@gmail.com", telefono = "6578-8951" 
+
+Where id_cliente = 5; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Rodrigo" , apellido = "Aviles ", correo = "rodriviles@yajoo.com", telefono = "6578-9687"  
+
+Where id_cliente = 7; 
+
+ 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Jhon" , apellido = "Sena", correo = "Senafamous@outlook.com", telefono = "7596-8745"  
+
+Where id_cliente = 9; 
+
+ 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Aldo" , apellido = "Valdez", correo = "valdez@outlook.com", telefono = "6578-9598"  
+
+Where id_cliente =11; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Gabriela" , apellido = "Monserrat", correo = "Gmonserrat@outlook.es", telefono = "6578-6577"  
+
+Where id_cliente = 13; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Calvin" , apellido = "Harris", correo = "calvinharris@outlook.es", telefono = "6578-9851"  
+
+Where id_cliente = 15; 
+
+ 
+
+
+
+ UPDATE CLIENTE 
+
+SET nombre = "Bruno" , apellido = "Mars", correo = "bruno@gmail.com", telefono = "6987-5475"  
+
+Where id_cliente = 17; 
+
+ 
+
+ 
+
+UPDATE CLIENTE 
+
+SET nombre = "Adam" , apellido = "Levine", correo = "Maroon5@outlook.com", telefono = "7489-9874"  
+
+Where id_cliente = 19; 
+
+ 
+
+UPDATE CLIENTE 
+
+SET nombre = "Michael" , apellido = "Jackson", correo = "mmjackson@gmail.com", telefono = "7489-6578"  
+
+Where id_cliente = 21; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "liam" , apellido = "gallager", correo = "Oasis@yajoo.com", telefono = "7795-6578"  
+
+Where id_cliente = 23; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Axel" , apellido = "Rose", correo = "gunsandroses@gmail.com", telefono = "7895-1432"  
+
+Where id_cliente = 25; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Cristian" , apellido = "Nodal", correo = "maltrago@gmail.com", telefono = "6546-4187"  
+
+Where id_cliente = 27; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Pepe" , apellido = "Aguilar", correo = "pepitojr@hotmail.com", telefono = "7845-9878"  
+
+Where id_cliente = 29; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Alejandro" , apellido = "Fernandez", correo = "alefernandez@gmail.com", telefono = "6547-7879"  
+
+Where id_cliente = 31; 
+
+ 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Luis" , apellido = "Miguel", correo = "elsoldemexico@outlook.com", telefono = "7898-7777"  
+
+Where id_cliente = 33; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Vicente" , apellido = "Fernadez", correo = "vicente@gmail.com", telefono = "7132-6578"  
+
+Where id_cliente = 35; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Katy" , apellido = "Perry", correo = "katiperri@gmail.es", telefono = "6578-7854"  
+
+Where id_cliente = 37; 
+
+
+
+UPDATE CLIENTE 
+
+SET nombre = "Juan Jose" , apellido = "de la cruz", correo = "jjdelacruz@gmail.com", telefono = "7896-4756"  
+
+Where id_cliente = 39; 
+
+UPDATE CLIENTE 
+
+SET nombre = "Pedro" , apellido = "Fernandez", correo = "jjdelacruz@gmail.com", telefono = "7123-4567"  
+
+Where id_cliente = 41; 
